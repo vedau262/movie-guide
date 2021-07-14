38f01d2bd2398c4043a792157d7c42910ef609b6
Dung TP <dung.tp@techchain.vn>
2021-07-14 11:16:44 +0700

    update

---
+++ netflix/lib/Base/extension/string_extension.dart
@@ -0,0 +1,37 @@
+extension StringExtension on String? {
+  int parseInt(){
+    return int.parse(this ?? "0");
+  }
+
+  double parseDouble() {
+    return double.parse(this ?? "0.0");
+  }
+
+  String getDefault(){
+    return this ?? "";
+  }
+}
+
+extension DynamicExtension on dynamic {
+  String parseToString() {
+    return (this as String).getDefault();
+  }
+  int parseToInt() {
+    return (this as int).getDefault();
+  }
+  double parseToDouble() {
+    return (this as double).getDefault();
+  }
+}
+
+extension IntExtension on int?{
+  int getDefault(){
+    return this ?? 0;
+  }
+}
+
+extension DoubleExtension on double? {
+  double getDefault(){
+    return this?? 0.0;
+  }
+}
